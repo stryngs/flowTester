ee0df39e1a0011faef5cbef17226c51af44213f1
stryngs <support@configitnow.com>
2021-02-28 23:18:44 -0500

    Math clarity and cleanup

--- getStats.py
+++ getStats.py
@@ -3,7 +3,8 @@
 import datetime
 from scapy.all import *
 
-## Obtain frame times
+# RX work
+## Obtain frame times from pcap
 fDict = {}
 fCount = 1
 rdr = PcapReader('mpTraffic.pcap')
@@ -13,34 +14,25 @@ with open('rx.stats', 'w') as oFile:
         oFile.write('{0} - {1}\n'.format(fCount, float(r.time)))
         fCount += 1
 
+## Time from start to finish
+span = len(fDict)
+eTime = fDict.get(span)
+sTime = fDict.get(1)
+eOut = datetime.fromtimestamp(int(eTime)).strftime("%Y-%m-%d %H:%M")
+sOut = datetime.fromtimestamp(int(sTime)).strftime("%Y-%m-%d %H:%M")
+tTime = eTime - sTime
+
+## Avg
+rAvg = tTime / span
+
+
+# TX work
 ## Notate log
 with open('flowtester.log') as iFile:
     fLog = iFile.read().splitlines()
 
-## Time from start to finish
-span = len(fDict)
-eTime = fDict.get(span)
-eOut = datetime.fromtimestamp(int(eTime)).strftime("%Y-%m-%d %H:%M")
-sTime = fDict.get(1)
-sOut = datetime.fromtimestamp(int(sTime)).strftime("%Y-%m-%d %H:%M")
-tTime = eTime - sTime
 
-
-## qty show
-print('\npcap length:')
-print(span)
-## Time compares
-print('\npcap timing:')
-print(sOut)
-print(eOut)
-
-## tX statistics
-print ('\ntx statistics:')
-print(fLog[2])
-
-print('\nrx statistics:')
-print('{0}\n'.format(tTime))
-
+# Compare work
 ## Delta
 ff = float(fLog[2])
 f = abs(ff)
@@ -49,20 +41,12 @@ t = abs(tt)
 pMax = max(f, t)
 if pMax == f:
     dt = f - t
-    val = 'tx higher'
+    val = 'tx higher value'
 else:
     dt = t - f
-    val = 'rx higher'
-print('tx -v- rx:')
-print('{0} - {1}\n'.format(str(dt), val))
+    val = 'rx higher value'
 
-print('tx interval:')
-print(fLog[3])
-
-print('\nrx average/sec:')
-rAvg = tTime / span
-print(str(rAvg) + '\n')
-
+## tx interval v rxAvg
 spd = float(fLog[3])
 aSpd = abs(spd)
 rSpd = abs(rAvg)
@@ -73,5 +57,54 @@ if rMax == aSpd:
 else:
     dt = rSpd - aSpd
     rVal = 'rx higher'
-print('txInt -v- rxAvg:')
-print('{0} - {1}\n'.format(str(dt), rVal))
+
+## tx rough v rxAvg
+rts = float(fLog[2]) / float(fLog[0])
+aRts = abs(rts)
+rMax = max(aRts, rSpd)
+if rMax == aRts:
+    adt = aRts - rSpd
+    arVal = 'tx higher'
+else:
+    adt = rSpd - aRts
+    arVal = 'rx higher'
+
+# Output
+## qty show
+print('\n~~~ RX Statistics ~~~')
+print('pcap length:')
+print(' {0}'.format(span))
+
+## time
+print('\nTotal time to receive:')
+print(' {0}'.format(tTime))
+
+## avg
+print('\nrx average/sec:')
+print(' {0}'.format(str(rAvg)))
+print ('~~~~~~~~~~~~~~~~~~~~~~\n')
+
+## tx statistics
+print('~~~ TX Statistics ~~~')
+print('Quantity sent:')
+print(' {0}'.format(fLog[0]))
+print ('\nTotal time to send:')
+print(' {0}\n'.format(fLog[2]))
+print('Send interval:')
+print(' {0}\n'.format(fLog[3]))
+print('Average time per send:')
+print(' {0}'.format(rts))
+print ('~~~~~~~~~~~~~~~~~~~~~~\n')
+
+## compares
+print('~~~ Comparisons ~~~')
+print('tx speed -v- rx speed:')
+print(' {0}\n {1}\n'.format(str(dt), val))
+
+print('tx interval -v- rx avg:')
+print(' {0}\n {1}\n'.format(str(dt), rVal))
+
+print('tx avg -v- rx avg:')
+print(' {0}\n {1}'.format(str(adt), arVal))
+print ('~~~~~~~~~~~~~~~~~~~~~~\n')
+
